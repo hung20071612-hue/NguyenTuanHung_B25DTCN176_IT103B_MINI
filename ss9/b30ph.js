c8f35931395cac777410c9dd2cde91bef18a6d2c
hung20071612-hue <hung20071612@gmail.com>
2026-02-25 13:49:25 +0700

    Add files via upload

---
+++ ss9/b30ph.js
@@ -0,0 +1,13 @@
+let orders = [`Đơn hàng A`,`Đơn hàng B`,`Đơn hàng C`,`Đơn hàng D`,`Đơn hàng E`];
+let revenues = [1500, 2800, 1200, -500, 3200];
+let orderReports = orders.map((value,index) => {
+   return value[index] = `${value} mang về ${revenues[index]} USD`
+});
+console.log(orderReports);
+let filters = revenues.filter(value => value > 0);
+console.log(filters);
+let total = filters.reduce((result,value) => {
+    return result + value;
+},0)
+console.log(total);
+
